87cf63d0f2b293511d1505c5d71a49bbf9e4a554
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-04-23 13:58:16 +0200

    tests: fix style applying suggestions from shellcheck

--- tests/functions.sh
+++ tests/functions.sh
@@ -1,50 +1,50 @@
 #!/usr/bin/env bash
 
 function wait_for_resource() {
-  local namespace="$1"
-  local time="$2"
-  local resource="$3"
+  local namespace="${1}"
+  local time="${2}"
+  local resource="${3}"
   
   local substring="AGE"
 
-  for i in $(seq 0 $time)
+  for i in $(seq 0 "${time}")
   do
-    get="$(kubectl get -n $namespace $resource)"
-    if [[ ! $get =~ $substring ]]; then
-      echo "Waiting for $resource in $i iteration"
+    get="$(kubectl get -n "${namespace}" "${resource}")"
+    if [[ ! ${get} =~ ${substring} ]]; then
+      echo "Waiting for ${resource} in ${i} iteration"
       sleep 1
     else
-      echo "Found $resource"
+      echo "Found ${resource}"
       break
     fi
   done
 }
 
 function wait_for_ns_termination() {
-  local namespace="$1"
-  local time="$2"
+  local namespace="${1}"
+  local time="${2}"
   
   local substring="AGE"
 
-  for i in $(seq 0 $time)
+  for i in $(seq 0 "${time}")
   do
-    get="$(kubectl get ns $namespace)"
-    if [[ $get =~ $substring ]]; then
-      echo "Waiting for $namespace termination in $i iteration"
+    get="$(kubectl get ns  \""${namespace}"\")"
+    if [[ ${get} =~ ${substring} ]]; then
+      echo "Waiting for \"${namespace}\" termination in ${i} iteration"
       sleep 1
     else
-      echo "$namespace terminated"
+      echo "${namespace} terminated"
       break
     fi
   done
 }
 
 function check_diff(){
-    diff="$1"
-    if [[ $diff == "" ]]; then
+    diff="${1}"
+    if [[ ${diff} == "" ]]; then
         echo "Test passed"
     else
-        echo $diff
+        echo "${diff}"
         echo "Test failed"
         exit 1
     fi
