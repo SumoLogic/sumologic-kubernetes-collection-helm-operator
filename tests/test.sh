a2058f5a3e69c6ae5e5a89f45d5393f017e1c2a7
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-04-23 13:58:16 +0200

    tests: fix style applying suggestions from shellcheck

--- tests/test.sh
+++ tests/test.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-echo "$(date)"
+date
 
 readonly ROOT_DIR="$(dirname "$(dirname "${0}")")"
-source "${ROOT_DIR}"/tests/functions.sh
 
-readonly IMG=""
+# shellcheck disable=SC1090
+source "${ROOT_DIR}/tests/functions.sh"
+
+readonly IMG="public.ecr.aws/sumologic/sumologic-kubernetes-collection-helm-operator:0.0.0"
 readonly NAMESPACE="sumologic-system"
 readonly TIME=900
 
@@ -35,56 +37,56 @@ helm install test-openshift sumologic/sumologic \
   -n sumologic-system \
   --create-namespace
 
-wait_for_resource ${NAMESPACE} ${TIME} daemonset.apps/test-openshift-falco
-wait_for_resource ${NAMESPACE} ${TIME} daemonset.apps/test-openshift-fluent-bit
-wait_for_resource ${NAMESPACE} ${TIME} daemonset.apps/test-openshift-prometheus-node-exporter 
+wait_for_resource "${NAMESPACE}" "${TIME}" daemonset.apps/test-openshift-falco
+wait_for_resource "${NAMESPACE}" "${TIME}" daemonset.apps/test-openshift-fluent-bit
+wait_for_resource "${NAMESPACE}" "${TIME}" daemonset.apps/test-openshift-prometheus-node-exporter 
 
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-kube-promet-operator
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-kube-state-metrics  
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-metrics-server
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-tailing-sidecar-operator
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-telegraf-operator
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-kube-promet-operator
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-kube-state-metrics  
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-metrics-server
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-tailing-sidecar-operator
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-telegraf-operator
 
-wait_for_resource ${NAMESPACE} ${TIME} statefulset.apps/prometheus-test-openshift-kube-promet-prometheus 
-wait_for_resource ${NAMESPACE} ${TIME} statefulset.apps/test-openshift-sumologic-fluentd-events
-wait_for_resource ${NAMESPACE} ${TIME} statefulset.apps/test-openshift-sumologic-fluentd-logs
-wait_for_resource ${NAMESPACE} ${TIME} statefulset.apps/test-openshift-sumologic-fluentd-metrics
+wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/prometheus-test-openshift-kube-promet-prometheus 
+wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/test-openshift-sumologic-fluentd-events
+wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/test-openshift-sumologic-fluentd-logs
+wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/test-openshift-sumologic-fluentd-metrics
 
-helm get manifest -n ${NAMESPACE}  test-openshift > helm_chart_templates.yaml
+helm get manifest -n "${NAMESPACE}"  test-openshift > helm_chart_templates.yaml
 
-helm delete test-openshift -n${NAMESPACE}
-kubectl delete ns ${NAMESPACE}
-wait_for_ns_termination ${NAMESPACE} ${TIME}
+helm delete test-openshift -n"${NAMESPACE}"
+kubectl delete ns "${NAMESPACE}"
+wait_for_ns_termination "${NAMESPACE}" "${TIME}"
 
-make deploy IMG=public.ecr.aws/sumologic/sumologic-kubernetes-collection-helm-operator:0.0.0
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/sumologic-controller-manager
+make deploy IMG="${IMG}"
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/sumologic-controller-manager
 kubectl wait --for=condition=ready --timeout 300s pod -l control-plane=controller-manager -n sumologic-system
 
 kubectl apply -f tests/test_openshift.yaml -n sumologic-system
 
-wait_for_resource ${NAMESPACE} ${TIME} daemonset.apps/test-openshift-falco
-wait_for_resource ${NAMESPACE} ${TIME} daemonset.apps/test-openshift-fluent-bit
-wait_for_resource ${NAMESPACE} ${TIME} daemonset.apps/test-openshift-prometheus-node-exporter 
+wait_for_resource "${NAMESPACE}" "${TIME}" daemonset.apps/test-openshift-falco
+wait_for_resource "${NAMESPACE}" "${TIME}" daemonset.apps/test-openshift-fluent-bit
+wait_for_resource "${NAMESPACE}" "${TIME}" daemonset.apps/test-openshift-prometheus-node-exporter 
 
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-kube-promet-operator
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-kube-state-metrics  
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-metrics-server
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-tailing-sidecar-operator
-wait_for_resource ${NAMESPACE} ${TIME} deployment.apps/test-openshift-telegraf-operator
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-kube-promet-operator
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-kube-state-metrics  
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-metrics-server
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-tailing-sidecar-operator
+wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-telegraf-operator
 
-wait_for_resource ${NAMESPACE} ${TIME} statefulset.apps/prometheus-test-openshift-kube-promet-prometheus 
-wait_for_resource ${NAMESPACE} ${TIME} statefulset.apps/test-openshift-sumologic-fluentd-events
-wait_for_resource ${NAMESPACE} ${TIME} statefulset.apps/test-openshift-sumologic-fluentd-logs
-wait_for_resource ${NAMESPACE} ${TIME} statefulset.apps/test-openshift-sumologic-fluentd-metrics
+wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/prometheus-test-openshift-kube-promet-prometheus 
+wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/test-openshift-sumologic-fluentd-events
+wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/test-openshift-sumologic-fluentd-logs
+wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/test-openshift-sumologic-fluentd-metrics
 
-helm get manifest -n ${NAMESPACE} test-openshift > helm_operator_templates.yaml
+helm get manifest -n "${NAMESPACE}" test-openshift > helm_operator_templates.yaml
 
 kubectl delete -f tests/test_openshift.yaml -n sumologic-system
 make undeploy
-kubectl delete ns ${NAMESPACE}
-wait_for_ns_termination ${NAMESPACE} ${TIME}
+kubectl delete ns "${NAMESPACE}"
+wait_for_ns_termination "${NAMESPACE}" "${TIME}"
 
-echo "$(date)"
+date
 
 # Remove lines which should be different from templates
 sed -i.bak '/checksum\/config:/d' helm_operator_templates.yaml
