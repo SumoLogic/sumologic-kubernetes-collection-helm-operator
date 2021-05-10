c3fc6f04ab7d13805e17d52c9b062163dc3ddd3f
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-05-10 09:22:50 +0200

    tests: split test script to enable running desired part

--- tests/deploy_helm_chart.sh
+++ tests/deploy_helm_chart.sh
@@ -7,13 +7,8 @@ readonly ROOT_DIR="$(dirname "$(dirname "${0}")")"
 # shellcheck disable=SC1090
 source "${ROOT_DIR}/tests/functions.sh"
 
-check_yq_version
-
-readonly IMG="public.ecr.aws/sumologic/sumologic-kubernetes-collection-helm-operator:0.0.4"
 readonly NAMESPACE="sumologic-system"
 readonly TIME=900
-
-kubectl apply -f https://raw.githubusercontent.com/SumoLogic/sumologic-kubernetes-collection/main/vagrant/k8s/receiver-mock.yaml
 
 helm repo add sumologic https://sumologic.github.io/sumologic-kubernetes-collection
 helm repo update
@@ -58,43 +53,3 @@ helm get manifest -n "${NAMESPACE}"  test-openshift > helm_chart_templates.yaml
 helm delete test-openshift -n"${NAMESPACE}"
 kubectl delete ns "${NAMESPACE}"
 wait_for_ns_termination "${NAMESPACE}" "${TIME}"
-
-make deploy IMG="${IMG}"
-wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/sumologic-controller-manager
-kubectl wait --for=condition=ready --timeout 300s pod -l control-plane=controller-manager -n sumologic-system
-
-kubectl apply -f tests/test_openshift.yaml -n sumologic-system
-
-wait_for_resource "${NAMESPACE}" "${TIME}" daemonset.apps/test-openshift-falco
-wait_for_resource "${NAMESPACE}" "${TIME}" daemonset.apps/test-openshift-fluent-bit
-wait_for_resource "${NAMESPACE}" "${TIME}" daemonset.apps/test-openshift-prometheus-node-exporter 
-
-wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-kube-promet-operator
-wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-kube-state-metrics  
-wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-metrics-server
-wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-tailing-sidecar-operator
-wait_for_resource "${NAMESPACE}" "${TIME}" deployment.apps/test-openshift-telegraf-operator
-
-wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/prometheus-test-openshift-kube-promet-prometheus 
-wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/test-openshift-sumologic-fluentd-events
-wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/test-openshift-sumologic-fluentd-logs
-wait_for_resource "${NAMESPACE}" "${TIME}" statefulset.apps/test-openshift-sumologic-fluentd-metrics
-
-helm get manifest -n "${NAMESPACE}" test-openshift > helm_operator_templates.yaml
-
-kubectl delete -f tests/test_openshift.yaml -n sumologic-system
-make undeploy
-kubectl delete ns "${NAMESPACE}"
-wait_for_ns_termination "${NAMESPACE}" "${TIME}"
-
-date
-
-# Remove lines which should be different from templates
-sed -i.bak '/checksum\/config:/d' helm_operator_templates.yaml
-sed -i.bak '/checksum\/config:/d' helm_chart_templates.yaml
-sed -i.bak '/caBundle:/d' helm_operator_templates.yaml
-sed -i.bak '/caBundle:/d' helm_chart_templates.yaml
-
-
-DIFF="$(diff <(yq e -P helm_operator_templates.yaml) <(yq e -P helm_chart_templates.yaml) )"
-check_diff "${DIFF}"
