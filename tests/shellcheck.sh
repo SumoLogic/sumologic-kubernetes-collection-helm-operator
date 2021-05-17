4ea589355156a81d20d44a4ff559765965f7c9e8
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-05-17 15:44:40 +0200

    ci: enable shellcheck for all shell scripts in repository

--- tests/shellcheck.sh
+++ tests/shellcheck.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Checking the bash scripts with shellcheck..."
-find . -path "*/tests/*" ! -path "*/helm-charts/*" -name '*.sh' -type 'f' -print |
+find . ! -path "*/helm-charts/*" -name '*.sh' -type 'f' -print |
     while read -r file; do
         # Run tests in their own context
         echo "Checking ${file} with shellcheck"
