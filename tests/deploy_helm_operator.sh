59e8a5f64ff677f689c64edc0fe3b0a8cbd0ecd2
Katarzyna Kujawa <kkujawa@sumologic.com>
2021-08-18 18:02:17 +0200

    release: prepare 2.1.4-0

--- tests/deploy_helm_operator.sh
+++ tests/deploy_helm_operator.sh
@@ -8,7 +8,7 @@ readonly ROOT_DIR="$(dirname "$(dirname "${0}")")"
 source "${ROOT_DIR}/tests/functions.sh"
 
 readonly DEPLOYMENT_TYPE="${DEPLOYMENT_TYPE:=default}"
-readonly IMG="${IMG:=public.ecr.aws/sumologic/sumologic-kubernetes-collection-helm-operator:2.1.4-0-rc.3}"
+readonly IMG="${IMG:=public.ecr.aws/sumologic/sumologic-kubernetes-collection-helm-operator:2.1.4-0}"
 readonly NAMESPACE="sumologic-system"
 readonly TIME=900
 
@@ -33,7 +33,7 @@ sed -i.bak "s#registry.connect.redhat.com/sumologic/sumo-ubi-minimal@#public.ecr
 sed -i.bak "s#registry.connect.redhat.com/sumologic/opentelemetry-collector@#public.ecr.aws/sumologic/opentelemetry-collector#g" bundle.yaml
 
 # Change image in bundle.yaml to imag
-sed -i.bak "s#public.ecr.aws/sumologic/sumologic-kubernetes-collection-helm-operator:2.1.4-0-rc.3#${IMG}#g" bundle.yaml
+sed -i.bak "s#registry.connect.redhat.com/sumologic/sumologic-kubernetes-collection-helm-operator:2.1.4-0#${IMG}#g" bundle.yaml
 
 kubectl apply -f bundle.yaml
 
