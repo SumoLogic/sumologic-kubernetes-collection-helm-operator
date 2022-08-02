bbfbb8a547c1e225554ed6c830dbddf88b0aad39
Katarzyna Kujawa <kkujawa@sumologic.com>
2022-08-02 15:37:23 +0200

    chore: change container images to images available in public registries to test Helm Operator with new images

--- tests/compare_manifests.sh
+++ tests/compare_manifests.sh
@@ -16,12 +16,12 @@ sed -i.bak '/caBundle:/d' helm_operator_templates.yaml
 sed -i.bak '/caBundle:/d' helm_chart_templates.yaml
 
 # Remove line with tag in Prometheus Spec
-sed -i.bak '/tag: v2.22.1-ubi/d' helm_operator_templates.yaml
+sed -i.bak '/tag: v2.22.1/d' helm_operator_templates.yaml
 # Remove tag from image in Prometheus Spec
-sed -i.bak 's#public.ecr.aws/sumologic/prometheus:v2.22.1-ubi#public.ecr.aws/sumologic/prometheus#g' helm_chart_templates.yaml
+sed -i.bak 's#quay.io/prometheus/prometheus:v2.22.1#quay.io/prometheus/prometheus#g' helm_chart_templates.yaml
 
 # Change image for telegraf operator to version with tag
-sed -i.bak 's#public.ecr.aws/sumologic/telegraf-operator-ubi@sha256:3a63153408ff6bb5d6294fcdedfa30b07a5023ca5f72dd63c6e4de31f6bf1c68#public.ecr.aws/sumologic/telegraf-operator-ubi:v1.3.5#g' helm_operator_templates.yaml
+sed -i.bak 's#quay.io/influxdb/telegraf-operator@sha256:20af9e3715f24002a23d3759b451ede47061fb27761e244f69bbe742f7a502d0#quay.io/influxdb/telegraf-operator@sha256:20af9e3715f24002a23d3759b451ede47061fb27761e244f69bbe742f7a502d0:v1.3.5#g' helm_operator_templates.yaml
 
 # busybox image has not tag set in collection
 sed -i.bak 's/busybox:1.33.0/busybox/g' helm_operator_templates.yaml
