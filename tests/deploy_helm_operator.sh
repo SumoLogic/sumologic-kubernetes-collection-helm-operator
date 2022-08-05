07ae2fc80f2d374d8105107fa60a98f615136fc6
Katarzyna Kujawa <kkujawa@sumologic.com>
2022-08-05 11:26:30 +0200

    fix: change operator image to publicly available image new version in `registry.connect.redhat.com` is not available at this moment

--- tests/deploy_helm_operator.sh
+++ tests/deploy_helm_operator.sh
@@ -33,7 +33,7 @@ readonly TIME=900
 # sed -i.bak "s#registry.connect.redhat.com/sumologic/opentelemetry-collector@#public.ecr.aws/sumologic/opentelemetry-collector#g" bundle.yaml
 
 # Change image in bundle.yaml to imag
-sed -i.bak "s#registry.connect.redhat.com/sumologic/sumologic-kubernetes-collection-helm-operator:2.14.1-0-rc.0#${IMG}#g" bundle.yaml
+sed -i.bak "s#public.ecr.aws/sumologic/sumologic-kubernetes-collection-helm-operator:2.14.1-0-rc.0#${IMG}#g" bundle.yaml
 
 kubectl apply -f bundle.yaml
 
