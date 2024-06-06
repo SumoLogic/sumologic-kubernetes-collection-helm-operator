cd49d707b374cea761a39dd1bcfa97233cf5c888
Dominik Rosiek <drosiek@sumologic.com>
2024-06-06 09:23:09 +0200

    chore: add vagrant for arm
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,6 +3,8 @@
 set -ex
 
 export DEBIAN_FRONTEND=noninteractive
+ARCH="$(dpkg --print-architecture)"
+
 apt-get update
 apt-get --yes upgrade
 
@@ -11,7 +13,7 @@ apt-get install --yes make gcc
 # Install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=${ARCH}] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 apt-get install --yes docker-ce docker-ce-cli containerd.io
@@ -38,15 +40,15 @@ echo "export KUBECONFIG=/var/snap/microk8s/current/credentials/kubelet.config" >
 
 # Install go
 GO_VERSION="1.22.4"
-wget "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
-tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
-rm "go${GO_VERSION}.linux-amd64.tar.gz"
+wget "https://golang.org/dl/go${GO_VERSION}.linux-${ARCH}.tar.gz"
+tar -C /usr/local -xzf "go${GO_VERSION}.linux-${ARCH}.tar.gz"
+rm "go${GO_VERSION}.linux-${ARCH}.tar.gz"
 echo "export PATH=${PATH}:/usr/local/go/bin" >> /home/vagrant/.bashrc
 
 # Install operator SDK
-curl -LO "https://github.com/operator-framework/operator-sdk/releases/latest/download/operator-sdk_linux_amd64"
-chmod +x operator-sdk_linux_amd64
-mv operator-sdk_linux_amd64 /usr/local/bin/operator-sdk
+curl -LO "https://github.com/operator-framework/operator-sdk/releases/latest/download/operator-sdk_linux_${ARCH}"
+chmod +x "operator-sdk_linux_${ARCH}"
+mv "operator-sdk_linux_${ARCH}" /usr/local/bin/operator-sdk
 
 # Install kustomize
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
@@ -55,8 +57,8 @@ mv kustomize /usr/local/bin/
 # Install Helm
 HELM_VERSION=v3.15.1
 mkdir /opt/helm3
-curl "https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz" | tar -xz -C /opt/helm3
-ln -s /opt/helm3/linux-amd64/helm /usr/bin/helm3
+curl "https://get.helm.sh/helm-${HELM_VERSION}-linux-${ARCH}.tar.gz" | tar -xz -C /opt/helm3
+ln -s "/opt/helm3/linux-${ARCH}/helm" /usr/bin/helm3
 ln -s /usr/bin/helm3 /usr/bin/helm
 
 SHELLCHECK_VERSION=v0.7.1
@@ -65,9 +67,9 @@ cp "shellcheck-${SHELLCHECK_VERSION}/shellcheck" /usr/local/bin
 rm -rf "shellcheck-${SHELLCHECK_VERSION}/"
 
 YQ_VERSION=v4.44.1
-wget "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64" -O /usr/bin/yq &&\
+wget "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_${ARCH}" -O /usr/bin/yq &&\
     chmod +x /usr/bin/yq
 
 OPM_VERSION=v1.43.1
-wget -q "https://github.com/operator-framework/operator-registry/releases/download/${OPM_VERSION}/linux-amd64-opm" -O /usr/bin/opm &&\
+wget -q "https://github.com/operator-framework/operator-registry/releases/download/${OPM_VERSION}/linux-${ARCH}-opm" -O /usr/bin/opm &&\
    chmod +x /usr/bin/opm
