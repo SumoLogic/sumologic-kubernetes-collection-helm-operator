cae51da99b731078f42016687fbf3688a3144b1e
Samiur Arif <samiur.arif@sumologic.com>
2025-06-23 14:41:02 -0700

    testing with 4.9

--- tests/helm_install.sh
+++ tests/helm_install.sh
@@ -3,7 +3,7 @@
 readonly ROOT_DIR="$(dirname "$(dirname "${0}")")"
 
 helm upgrade --install test-openshift sumologic/sumologic \
-  --version 4.13.0 \
+  --version 4.9.0 \
   -n sumologic-system \
   --create-namespace -f "${ROOT_DIR}/tests/values.yaml" \
   --set instrumentation.instrumentationJobImage.image.repository=public.ecr.aws/sumologic/kubernetes-tools-kubectl@sha256 \
